589f0ecb5c016baeffaaff089b78127eec9204d7
Owen Garland <owen.garland@clock.co.uk>
2016-10-20 12:52:58 +0100

    Added tab navigation, and home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root 'homes#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014153835) do
+ActiveRecord::Schema.define(version: 20161020114747) do
+
+  create_table "homes", force: :cascade do |t|
+    t.string   "welcomeText"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
